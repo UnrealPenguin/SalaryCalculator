6047a84dc3171b7a20e0ee48fd6b8596e38b5d41
UnrealPenguin <ericng1077@gmail.com>
2022-10-11 10:14:22 -0400

    updated display for MAC

--- Display.py
+++ Display.py
@@ -33,13 +33,13 @@ class Display():
         attendanceLabel = Label(container, text="Attendance Link:", bg=c.BGCOLOR, fg="white")
         self.attendanceInput = Entry(container)
         attendanceLabel.place(x=50, y=200)
-        self.attendanceInput.place(x=145, y=200)
+        self.attendanceInput.place(x=170, y=200)
 
         # FOR TAX DATA
         taxLabel = Label(container, text="Tax Link:", bg=c.BGCOLOR, fg="white")
         self.taxInput = Entry(container)
         taxLabel.place(x=50, y=230)
-        self.taxInput.place(x=145, y=230)
+        self.taxInput.place(x=170, y=230)
 
         # FOR EXCEPTIONS
         step2 = Label(container, text="Step 2", bg=c.BGCOLOR, fg="white", font=(None, 18))
@@ -54,17 +54,17 @@ class Display():
         dayOffLabel = Label(container, text="Day Off:", bg=c.BGCOLOR, fg="white")
         self.dayOffInput = Entry(container)
         dayOffLabel.place(x=50, y=380)
-        self.dayOffInput.place(x=145, y=380)
+        self.dayOffInput.place(x=170, y=380)
 
         # FOR HOLIDAY EXCEPTION
         holidayLabel = Label(container, text="Holiday:", bg=c.BGCOLOR, fg="white")
         self.holidayInput = Entry(container)
         holidayLabel.place(x=50, y=410)
-        self.holidayInput.place(x=145, y=410)
+        self.holidayInput.place(x=170, y=410)
 
         # FOR DISPLAYING THE NEW DF ON CLICK
         btnCalculate = Button(container, text="Calculate Salary", command=self.displayDF)
-        btnCalculate.place(x=850, y=950)
+        btnCalculate.place(x=750, y=650)
 
 
     def getData(self):
@@ -96,11 +96,6 @@ class Display():
         if(holidayDate):
             holiday = holidayDate.replace(" ", "").split(",")
             exception.extend(process.createExceptions(holiday, 1))
-
-        # exception = [{"date":10, "exception":"dayOff"}, {"date":12,"exception":"dayOff"}, {"date":13,"exception":"dayOff"}]
-        # # note-to-self if not empty append, else create the array
-        # exception.extend([{"date":27, "exception":"dayOff"}, {"date":28, "exception":"dayOff"}])
-        # exception.extend([{"date":26, "exception":"hourly"}, {"date":29, "exception":"hourly"}, {"date":30, "exception":"hourly"}])
 
         process.calculateAttendance(exception)
 
